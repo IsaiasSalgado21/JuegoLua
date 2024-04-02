a4b7abdd3800b200e58a613750c9d6b828b70a06
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-02 04:48:22 -0700

    primer commit para iniciar el repositorio de lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+--suma con variables
+a=5
+b=8
+print(a+b)
+
+-- juntar strings con variables de tipo texto
+nombre = "isaias salgado"
+edad = "22"
+print("hola, mi nombre es" .. nombre .. "mi edad es de " .. edad) 
+--[[comentario 
+de varias 
+lineas en lua]]
