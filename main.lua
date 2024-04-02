ca85864beeea41d369e0f397f7d1423530cb713f
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-02 14:42:00 -0700

    ahora añadimos unas funciones de love2d para dibujar un rectangulo y moverlo hacia la derecha sumando +5 a la cordenada x

--- main.lua
+++ main.lua
@@ -34,12 +34,29 @@ print(suma(232,89))
 
 print("----------------------------")
 
---funcion para dibujar un hola mundo en love2d
+--[[funcion para dibujar un hola mundo en love2d
 function love.draw()
     love.graphics.print("Hello World!", 400, 300)
     end
 
---funcion para dibujar un rectangulo
+--funcion para dibujar un rectangulo con color por dentro
 function love.draw()
-    love.graphics.rectangle("line", 100, 200, 50, 90)
+    love.graphics.rectangle("fill", 100, 200, 50, 90)
 end
+]]
+
+
+--funciones principales para el movimiento
+function love.load()
+    x = 100
+end
+--con esta funcion se va a estar actualizando la coordenada x de nuestro rectangulo
+function love.update()
+    x = x + 5
+end
+--funcion para dibujar un rectangulo con lineas de color
+function love.draw()
+    love.graphics.rectangle("line", x, 50, 200, 150)
+end
+
+
