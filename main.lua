2deaa92cd5c120d6486b41754c0e725db692f09c
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-02 05:36:59 -0700

    utilece algunas funciones para regresar valores y otra para dibujar un cuadrado

--- main.lua
+++ main.lua
@@ -6,7 +6,40 @@ print(a+b)
 -- juntar strings con variables de tipo texto
 nombre = "isaias salgado"
 edad = "22"
-print("hola, mi nombre es" .. nombre .. "mi edad es de " .. edad) 
+print("hola, mi nombre es " .. nombre .. " mi edad es de " .. edad) 
 --[[comentario 
 de varias 
 lineas en lua]]
+print("----------------------------")
+-- funciones pasar un parametro 
+function saludo(num)
+    print("hola, me gusta el numero "..num)
+end
+
+saludo(23)
+saludo(24)
+saludo(67)
+-- funcion que devulve un valor con return
+function dameCinco()
+    return 5
+end
+cinco = dameCinco()
+print(cinco)
+
+--funcion que recibe argumentos y regresa la suma
+function suma(x, y)
+    return x + y
+end
+print(suma(232,89))
+
+print("----------------------------")
+
+--funcion para dibujar un hola mundo en love2d
+function love.draw()
+    love.graphics.print("Hello World!", 400, 300)
+    end
+
+--funcion para dibujar un rectangulo
+function love.draw()
+    love.graphics.rectangle("line", 100, 200, 50, 90)
+end
